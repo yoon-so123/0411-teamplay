eac6fa9cefad1d33a0a3a4daff2b151d00dbbac7
yoon-so123 <dadada2659@gmail.com>
2025-04-15 12:16:55 +0900

    view more
    기능 추가.

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ open.addEventListener("click", () => {menu.classList.add("move"
 );});
 close.addEventListener("click",() => {menu.classList.remove("move");});
 
+const toggleInput = document.getElementById("view-toggle");
+const giArticles = document.querySelectorAll(".gi");
+const footer = document.querySelector("footer");
+const viewMoreLabel = document.querySelector(".view-more");
+
+toggleInput.addEventListener("change", function () {
+  if (this.checked) {
+    giArticles.forEach((article) => article.classList.add("gimove"));
+
+    footer.classList.remove("footergo");
+
+    viewMoreLabel.style.display = "none";
+  } else {
+    giArticles.forEach((article) => article.classList.remove("gimove"));
+    footer.classList.add("footergo");
+    viewMoreLabel.style.display = "block";
+  }
+});
